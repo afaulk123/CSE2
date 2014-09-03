0ffbc5359c54888b8ed2de5633ed478198211276
afaulk123 <anf217@lehigh.edu>
2014-09-03 18:34:57 +0000

    reupload of hw01

---
+++ Labs/lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////
+//Ashley Faulkner
+//Lab 01
+//Hello World Java Program
+//
+//  first compile the the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  define the class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//      print the statement with box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!   #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//Ashley Faulkner
+//Hw01
+//Welcome Class Java Program
+//
+//  first compile the java program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define the class
+    public class WelcomeClass{
+    
+//  add the main method 
+    public static void main(String[] args) {
+        
+//      Print the following statement designated for homework 01
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-A--N--F--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("My name is Ashley Faulkner and I am a Sophomore from Cincinnati, Ohio. I am majoring in Industrial Systems Engineering");
+        
+    } //end main method
+} //end class
